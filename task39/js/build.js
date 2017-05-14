a7085f1caaa74183f78442c8d4843f3ef179cd4a
zbhgit <zbh_wj@163.com>
2017-05-15 00:37:55 +0800

    ubdate

---
+++ task39/js/build.js
@@ -0,0 +1,12 @@
+
+({
+    baseUrl: ".",
+    paths: {
+        'jquery': './lib/jquery',
+        'GoTop':'./com/GoTop',
+        'Carousel':'./com/Carousel',
+        'WaterFall':'./com/WaterFall'
+    },
+    name: "main",
+    out: "out/index.merge.min.js"
+});
